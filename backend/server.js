afece6921f033b5a0ecdbadac8f9a37918f4a19e
Amar G Nath <84448118+Amarr7777@users.noreply.github.com>
2024-09-30 13:21:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,19 @@ const port = process.env.PORT ||4000;
 
 //middleware
 app.use(express.json());
-app.use(cors({
-    origin: 'https://cloudkitchen-1.onrender.com',
+const allowedOrigins = [
+    'https://cloudkitchen-1.onrender.com',
+    'https://cloudkitchen-1-c492.onrender.com'
+  ];
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); 
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true); 
+      } else {
+        callback(new Error('Not allowed by CORS')); /
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true 
   }));
